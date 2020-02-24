a5f1250cc327f769527c1e5259e854c7b91bb8ba
swliddle <liddle@byu.edu>
2020-02-24 15:20:28 -0700

    Worked on reading initial data from a JSON file. This version is 90% there, but not 100%.

--- lib/model.dart
+++ lib/model.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Mentee {
   var id = "";
@@ -10,6 +13,14 @@ class Mentee {
 
   Mentee(this.id, this.lastName, this.firstName, this.cellPhone, this.email,
       this.activityLog);
+
+  Mentee.fromJson(Map<String, dynamic> json)
+      : this.id = json["id"],
+        this.firstName = json["firstName"],
+        this.lastName = json["lastName"],
+        this.cellPhone = json["cellPhone"],
+        this.email = json["email"],
+        this.activityLog = <ActivityRecord>[];
 
   String getField(String fieldName) {
     switch (fieldName) {
@@ -41,34 +52,46 @@ class ActivityRecord {
 }
 
 class MenteeModel extends ChangeNotifier {
-  var _mentees = <Mentee>[
-    Mentee("1", "Doe", "John", "801-555-1212",
-        "email@example.com", <ActivityRecord>[
-      ActivityRecord("1", "1", DateTime.parse("2020-01-02 08:18:04Z"), 30,
-          "Had a great time"),
-      ActivityRecord("2", "1", DateTime.parse("2020-01-03 09:19:06Z"), 15,
-          "Had a great time"),
-      ActivityRecord("3", "1", DateTime.parse("2020-01-04 10:20:09Z"), 30,
-          "Had a great time"),
-      ActivityRecord("4", "1", DateTime.parse("2020-01-05 11:21:13Z"), 15,
-          "Had a great time"),
-      ActivityRecord("5", "1", DateTime.parse("2020-01-06 12:22:17Z"), 30,
-          "Had a great time"),
-    ]),
-    Mentee("2", "Roe", "Mary", "801-555-1212",
-        "email@example.com", <ActivityRecord>[
-      ActivityRecord("1", "1", DateTime.parse("2020-01-07 12:18:04Z"), 30,
-          "Had a great time"),
-      ActivityRecord("2", "1", DateTime.parse("2020-01-08 13:28:14Z"), 15,
-          "Had a great time"),
-      ActivityRecord("3", "1", DateTime.parse("2020-01-09 14:38:24Z"), 30,
-          "Had a great time"),
-      ActivityRecord("4", "1", DateTime.parse("2020-01-10 15:48:34Z"), 15,
-          "Had a great time"),
-      ActivityRecord("5", "1", DateTime.parse("2020-01-11 16:58:44Z"), 30,
-          "Had a great time"),
-    ]),
-  ];
+  var _mentees = <Mentee>[];
+//  = <Mentee>[
+//    Mentee("1", "Doe", "John", "801-555-1212",
+//        "email@example.com", <ActivityRecord>[
+//      ActivityRecord("1", "1", DateTime.parse("2020-01-02 08:18:04Z"), 30,
+//          "Had a great time"),
+//      ActivityRecord("2", "1", DateTime.parse("2020-01-03 09:19:06Z"), 15,
+//          "Had a great time"),
+//      ActivityRecord("3", "1", DateTime.parse("2020-01-04 10:20:09Z"), 30,
+//          "Had a great time"),
+//      ActivityRecord("4", "1", DateTime.parse("2020-01-05 11:21:13Z"), 15,
+//          "Had a great time"),
+//      ActivityRecord("5", "1", DateTime.parse("2020-01-06 12:22:17Z"), 30,
+//          "Had a great time"),
+//    ]),
+//    Mentee("2", "Roe", "Mary", "801-555-1212",
+//        "email@example.com", <ActivityRecord>[
+//      ActivityRecord("1", "1", DateTime.parse("2020-01-07 12:18:04Z"), 30,
+//          "Had a great time"),
+//      ActivityRecord("2", "1", DateTime.parse("2020-01-08 13:28:14Z"), 15,
+//          "Had a great time"),
+//      ActivityRecord("3", "1", DateTime.parse("2020-01-09 14:38:24Z"), 30,
+//          "Had a great time"),
+//      ActivityRecord("4", "1", DateTime.parse("2020-01-10 15:48:34Z"), 15,
+//          "Had a great time"),
+//      ActivityRecord("5", "1", DateTime.parse("2020-01-11 16:58:44Z"), 30,
+//          "Had a great time"),
+//    ]),
+//  ];
+
+  MenteeModel() {
+    _getInitialData();
+  }
+
+  static void _getInitialData() async {
+    var jsonString = await rootBundle.loadString('assets/initial_data.json');
+    var jsonData = jsonDecode(jsonString);
+
+//    jsonData.forEach((jsonObject) {_mentees.add(Mentee.fromJson(jsonObject))});
+  }
 
   get mentees => _mentees;
   get activityRecords => _mentees
