762dc9a8a4a2dd6da3d82992620069cf2f714d1c
swliddle <liddle@byu.edu>
2020-02-24 15:30:54 -0700

    This version adds Mary Roe to the initial_data.json file and also finishes adding all mentees to the list (which was broken at the end of class 2/24/20).

--- lib/model.dart
+++ lib/model.dart
@@ -53,44 +53,18 @@ class ActivityRecord {
 
 class MenteeModel extends ChangeNotifier {
   var _mentees = <Mentee>[];
-//  = <Mentee>[
-//    Mentee("1", "Doe", "John", "801-555-1212",
-//        "email@example.com", <ActivityRecord>[
-//      ActivityRecord("1", "1", DateTime.parse("2020-01-02 08:18:04Z"), 30,
-//          "Had a great time"),
-//      ActivityRecord("2", "1", DateTime.parse("2020-01-03 09:19:06Z"), 15,
-//          "Had a great time"),
-//      ActivityRecord("3", "1", DateTime.parse("2020-01-04 10:20:09Z"), 30,
-//          "Had a great time"),
-//      ActivityRecord("4", "1", DateTime.parse("2020-01-05 11:21:13Z"), 15,
-//          "Had a great time"),
-//      ActivityRecord("5", "1", DateTime.parse("2020-01-06 12:22:17Z"), 30,
-//          "Had a great time"),
-//    ]),
-//    Mentee("2", "Roe", "Mary", "801-555-1212",
-//        "email@example.com", <ActivityRecord>[
-//      ActivityRecord("1", "1", DateTime.parse("2020-01-07 12:18:04Z"), 30,
-//          "Had a great time"),
-//      ActivityRecord("2", "1", DateTime.parse("2020-01-08 13:28:14Z"), 15,
-//          "Had a great time"),
-//      ActivityRecord("3", "1", DateTime.parse("2020-01-09 14:38:24Z"), 30,
-//          "Had a great time"),
-//      ActivityRecord("4", "1", DateTime.parse("2020-01-10 15:48:34Z"), 15,
-//          "Had a great time"),
-//      ActivityRecord("5", "1", DateTime.parse("2020-01-11 16:58:44Z"), 30,
-//          "Had a great time"),
-//    ]),
-//  ];
 
   MenteeModel() {
-    _getInitialData();
+    _getInitialData(_mentees);
   }
 
-  static void _getInitialData() async {
+  static void _getInitialData(List<Mentee> menteesList) async {
     var jsonString = await rootBundle.loadString('assets/initial_data.json');
     var jsonData = jsonDecode(jsonString);
 
-//    jsonData.forEach((jsonObject) {_mentees.add(Mentee.fromJson(jsonObject))});
+    jsonData.forEach((jsonObject) {
+      menteesList.add(Mentee.fromJson(jsonObject));
+    });
   }
 
   get mentees => _mentees;
