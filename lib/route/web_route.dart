717ffd0dd9f07110cbaf5f0479ed5d8899e43b0e
swliddle <liddle@byu.edu>
2020-03-19 14:12:16 -0600

    This version shows how to load custom HTML into the WebView in place of a URL for it to load over the network.

--- lib/route/web_route.dart
+++ lib/route/web_route.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:mentor_tracking/widget/app_bar.dart';
 import 'package:webview_flutter/webview_flutter.dart';
@@ -10,7 +12,13 @@ class WebRoute extends StatelessWidget {
       body: Center(
         child: Container(
           child: WebView(
-            initialUrl: 'https://www.byu.edu',
+            initialUrl: Uri.dataFromString("""
+              <p>This is <b>dynamic</b> HTML that I've hard-coded into
+              my <code>WebView</code>.  Isn't it sweet?  Yes, <em>yes</em>,
+              I do believe it <em>is</em>.
+              </p>
+              """, mimeType: 'text/html', encoding: Encoding.getByName('utf-8'))
+                .toString(),
             javascriptMode: JavascriptMode.unrestricted,
           ),
         ),
